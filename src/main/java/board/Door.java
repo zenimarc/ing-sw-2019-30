00b74a1d0716c1e1ef3717dc2c9cf1ab5ee498c9
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-04-02 15:36:10 +0200

    add getter and constructor

--- src/main/java/board/Door.java
+++ src/main/java/board/Door.java
@@ -8,7 +8,16 @@ public class Door {
     private Cell cell1;
     private Cell cell2;
 
-    public Door() {
+    public Door(Cell c1, Cell c2) {
+        this.cell1 = c1;
+        this.cell2 = c2;
     }
 
+    public Cell getCell1() {
+        return cell1;
+    }
+
+    public Cell getCell2() {
+        return cell2;
+    }
 }
