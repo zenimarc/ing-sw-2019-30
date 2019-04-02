761a688c97acbee15c98b71818c70dfc84478156
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-04-02 17:07:25 +0200

    get distance(other position)

--- src/main/java/board/Position.java
+++ src/main/java/board/Position.java
@@ -1,5 +1,7 @@
 package board;
 
+import static java.lang.Math.abs;
+
 public class Position {
     private int x;
     private int y;
@@ -26,6 +28,11 @@ public class Position {
         if(x == p.x && (y==p.y-1 || y==p.y+1)){return true;}
         if(y == p.y && (x==p.x-1 || x==p.x+1)){return true;}
         return false;
+    }
+
+    //NB not see wall
+    public int distance(Position p){
+        return abs(this.x-p.x) + abs(this.y-p.y);
     }
 
     public int getX() {
