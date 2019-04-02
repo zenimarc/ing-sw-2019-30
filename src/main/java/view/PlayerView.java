ff9163f9cf6f1c395ae235662d7627dcfa2efd73
Marco <marco.verzeni@mail.polimi.it>
2019-04-02 16:23:40 +0200

    add generated code from UML view

---
+++ src/main/java/view/PlayerView.java
@@ -0,0 +1,78 @@
+package view;
+import board.Cell;
+import player.Player;
+import deck.WeaponCard;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class PlayerView {
+
+    /**
+     * Default constructor
+     */
+    public PlayerView() {
+    }
+
+    /**
+     * 
+     */
+    public Player player;
+
+    /**
+     * 
+     */
+    public PlayerBoardView myPlayerBoard;
+
+
+
+    public boolean move(Cell cell) {
+        // TODO implement here
+        return false;
+    }
+
+
+    public boolean grab(Cell cell) {
+        // TODO implement here
+        return false;
+    }
+
+
+    public boolean shoot(Cell cell, WeaponCard weapon) {
+        // TODO implement here
+        return false;
+    }
+
+
+    private boolean finalMove(Cell cell) {
+        // TODO implement here
+        return false;
+    }
+
+
+    private boolean finalGrab(Cell cell) {
+        // TODO implement here
+        return false;
+    }
+
+
+    private boolean finaShoot(Cell cell, WeaponCard weapon) {
+        // TODO implement here
+        return false;
+    }
+
+
+    public void drawCLI() {
+        // TODO implement here
+
+    }
+
+
+    public void drawGUI() {
+        // TODO implement here
+
+    }
+
+}
