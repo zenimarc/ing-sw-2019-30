a5cd2a8100365c2c87026d62a5428191974a2912
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-04-11 17:01:35 +0200

    no side-effect or require attack

---
+++ src/main/java/attack/SimpleAttack.java
@@ -0,0 +1,26 @@
+package attack;
+
+
+import player.Player;
+
+public class SimpleAttack extends Attack implements AttackInterface {
+
+    private static int damage;
+    private static int mark;
+
+
+    public SimpleAttack(String name, String description, int damage, int mark){
+        this.name = name;
+        this.description = description;
+        this.damage = damage;
+        this.mark = mark;
+    }
+
+    @Override
+    public boolean attack(Player player, Player opponents) {
+        opponents.addDamage(player,damage);
+        opponents.addMark(player, mark);
+    return true;
+    }
+
+}
