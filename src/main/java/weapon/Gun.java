c13821a6dfc5022e51e1d479a6694159c110b246
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-04-11 18:17:17 +0200

    new type of weapon

---
+++ src/main/java/weapon/Gun.java
@@ -0,0 +1,17 @@
+package weapon;
+
+import attack.Attack;
+import attack.SimpleAttack;
+
+import java.util.List;
+
+public class Gun extends WeaponCard {
+
+
+    public Gun(String name, int[] cost, List<Attack> attacks) {
+        super();
+    }
+
+
+
+}
