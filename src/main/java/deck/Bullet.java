8985672525a2ca407d9f4e48786bed7f941725fa
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-04-12 14:54:00 +0200

    new toIntArray(List of Bullets)

--- src/main/java/deck/Bullet.java
+++ src/main/java/deck/Bullet.java
@@ -21,4 +21,30 @@ public class Bullet {
      * End constructors
      */
 
+    /**
+     * From ArrayList of Bullet to int array[num of R,num of Y, num of B]
+     * @param bullets
+     * @return
+     */
+    public int[] toIntArray(ArrayList<Bullet> bullets){
+
+        int[] colorArray = new int[3];
+        int i;
+
+        for(Bullet bullet : bullets){
+            switch (bullet.color){
+                case RED:
+                    colorArray[0]++;
+                    break;
+                case YELLOW:
+                    colorArray[1]++;
+                    break;
+                case BLUE:
+                    colorArray[2]++;
+                    break;
+            }
+        }
+        return colorArray;
+    }
+
 }
