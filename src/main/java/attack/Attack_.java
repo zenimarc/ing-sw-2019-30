b2607d821a265347a090e142130608d4b798ce21
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-04-13 20:13:34 +0200

    add JavaDoc

--- src/main/java/attack/Attack_.java
+++ src/main/java/attack/Attack_.java
@@ -2,6 +2,10 @@ package attack;
 
 import player.Player;
 
+/**
+ * This class is an idea of genertic attack.
+ * Need implements different attack's type.
+ */
 public abstract class Attack_ {
 
     protected String name;
@@ -9,16 +13,33 @@ public abstract class Attack_ {
     protected int[] bulletsColor;
 
     public abstract boolean attack(Player player, Player opponent);
+
+    /**
+     * This return the attack's description
+     * @return attack's description
+     */
     public abstract String getDescription();
 
+    /**
+     * set how many bullets need to use this attack
+     * @param bulletsColor int[num of R, num of Y, num of B]
+     */
     public void setCost(int[] bulletsColor) {
         this.bulletsColor = bulletsColor;
     }
 
+    /**
+     * Get how many bullets need to use this attack
+     * @return int[num of R, num of Y, num of B]
+     */
     public int[] getCost() {
         return bulletsColor;
     }
 
+    /**
+     * Get attack's name
+     * @return attack's name
+     */
     public String getName(){return this.name;}
 
     }
--- src/main/java/attack/SingleAttack.java
+++ src/main/java/attack/SingleAttack.java
@@ -1,13 +1,16 @@
 package attack;
 
-
 import player.Player;
 
+/**
+ * Single attack is an attack that hit an opponent and add damage and/or mark to it
+ */
 public class SingleAttack extends Attack_{
 
     private int damage;
     private int mark;
 
+    //Constructor
     public SingleAttack(String name, String description, int damage, int mark){
         this.name = name;
         this.description = description;
@@ -21,7 +24,14 @@ public class SingleAttack extends Attack_{
         this.damage = damage;
         this.mark = mark;
     }
+    //End Constructor
 
+    /**
+     * Add damage and mark to opponent by player
+     * @param player Player that attack
+     * @param opponent Player hit
+     * @return true
+     */
     @Override
     public boolean attack(Player player, Player opponent) {
         opponent.addDamage(player,damage);
@@ -29,6 +39,10 @@ public class SingleAttack extends Attack_{
     return true;
     }
 
+    /**
+     * This return attack's description
+     * @return attack's description
+     */
     @Override
     public String getDescription() {
         StringBuilder sb = new StringBuilder();
@@ -40,10 +54,18 @@ public class SingleAttack extends Attack_{
         return sb.toString();
     }
 
+    /**
+     * Get attaks's damages
+     * @return attaks's damages
+     */
     public int getDamage() {
         return damage;
     }
 
+    /**
+     * Get attak's marks
+     * @return attak's marks
+     */
     public int getMark() {
         return mark;
     }
