008cb35b0cb576546304e450488517ce4fc9f3b4
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-04-28 20:14:24 +0200

    finished MACHINEGUN

--- src/main/java/weapon/Gun.java
+++ src/main/java/weapon/Gun.java
@@ -26,34 +26,58 @@ public class Gun extends WeaponCard{
         }
     }
 
+    private boolean lockrifleShot(int typeAttack, Player shooter, List<Player> opponents){
+        //Base Attack
+        attacks.get(0).attack(shooter, opponents.get(0));
+        //Optional attack
+        if(typeAttack==1) {
+            if (shooter.useAmmo(attacks.get(1).getCost())) {
+                attacks.get(1).attack(shooter, opponents.get(1));
+                return true;
+            } else return false;
+        }
+        return true;
+    }
+
+    private boolean machinegunShot(int typeAttack, Player shooter, List<Player> opponents){
+        //Base Attack
+        attacks.get(0).attack(shooter,opponents);
+
+        //Optional attack
+        switch (typeAttack){
+            case 1:
+                if(shooter.useAmmo(attacks.get(1).getCost())){
+                    attacks.get(1).attack(shooter, opponents.get(0));
+                }else return false;
+                break;
+            case 2:
+                if(shooter.useAmmo(attacks.get(2).getCost())){
+                    attacks.get(2).attack(shooter,opponents.subList(1,2));
+                }else return false;
+                break;
+            case 12:
+                if(shooter.canPay(attacks.get(1).getCost()) && shooter.canPay(attacks.get(2).getCost())){
+                    machinegunShot(1,shooter,opponents);
+                    machinegunShot(2,shooter,opponents);
+                }else return false;
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+
     @Override
     public boolean shoot(int typeAttack, Player shooter, List<Player> opponents) {
         if(typeAttack>=attacks.size()) return false;
         switch (this.weaponType) {
             case LOCKRIFLE:
-                switch (typeAttack) {
-                    case 0:
-                        //Base Attack
-                        attacks.get(0).attack(shooter, opponents.get(0));
-                        return true;
-                    case 1:
-                        //Optional Attack
-                        if (shooter.useAmmo(attacks.get(1).getCost())) {
-                            attacks.get(0).attack(shooter, opponents.get(0));
-                            attacks.get(1).attack(shooter, opponents.get(1));
-                            return true;
-                        }
-                        break;
-                    default:
-                        return false;
-                }
-                break;
+                return lockrifleShot(typeAttack,shooter,opponents);
             case MACHINEGUN:
-                break;
-
+                return machinegunShot(typeAttack,shooter,opponents);
             default:
                 return false;
         }
-        return false;
     }
 }
