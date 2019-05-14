35cd88408ef0be4c5a220516bac55d7226e17356
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-05-14 17:35:20 +0200

    change weaponfactory

--- src/main/java/weapon/EnumSimpleWeapon.java
+++ src/main/java/weapon/EnumSimpleWeapon.java
@@ -7,18 +7,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public enum EnumWeapon {
+public enum EnumSimpleWeapon {
+
     LOCK_RIFLE("Lockrifle", new ArrayList<>(Arrays.asList(new Bullet(Color.BLUE),new Bullet(Color.BLUE)))),
     MACHINE_GUN("Machinegun", new ArrayList<>(Arrays.asList(new Bullet(Color.BLUE), new Bullet(Color.RED)))),
     ELECTROSCYTHE("Electroscyte", new ArrayList<>(Arrays.asList(new Bullet(Color.BLUE)))),
     HEATSEEKER("Heatseeker", new ArrayList<>(Arrays.asList(new Bullet(Color.RED),new Bullet(Color.RED),new Bullet(Color.YELLOW)))),
-    ;
+    ZX_2("ZX-2",new ArrayList<>(Arrays.asList(new Bullet(Color.YELLOW),new Bullet(Color.RED))) );
 
 
     private String name;
     private ArrayList<Bullet> cost;
 
-    EnumWeapon(String name, ArrayList<Bullet> bullets){
+    EnumSimpleWeapon(String name, ArrayList<Bullet> bullets){
         this.name = name;
         this.cost = bullets;
     }
@@ -27,5 +28,3 @@ public enum EnumWeapon {
 
     public List<Bullet> getCost() { return cost;}
 }
-
-
