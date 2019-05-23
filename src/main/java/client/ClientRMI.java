10fd9989cbbe7a8556055c7346522d8b13a1fd8f
Marco <marco.verzeni@mail.polimi.it>
2019-05-23 22:29:59 +0200

    implemented basic rmi communication

--- src/main/java/client/ClientRMI.java
+++ src/main/java/client/ClientRMI.java
@@ -1,14 +1,59 @@
 package client;
 
-public class ClientRMI implements Client {
-    private String gameId;
+import server.Lobby;
+import server.LobbyImpl;
 
-    public ClientRMI(){
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ClientRMI extends UnicastRemoteObject implements Client {
+    private String gameId;
+    private String nickname;
+    private Registry registry;
+
+    public ClientRMI(String nickname) throws RemoteException{
+        this.nickname = nickname;
     }
+
 
     public void gameStarted(Object obj){
         this.gameId = (String) obj;
         System.out.println("pronti a giocare nel server: "+ gameId);
     }
 
+    public void login(String nickname, String ip) throws RemoteException{
+        login(ip, 1099);
+    }
+    public void login(String host, int port) throws RemoteException {
+        this.registry = LocateRegistry.getRegistry(host, port);
+        try {
+            Lobby lobby = (Lobby) registry.lookup("lobby");
+            lobby.login(this.nickname, this);
+        }catch (NotBoundException nbe){
+            nbe.fillInStackTrace();
+        }
+
+
+    }
+
+    public void loggedIn(){
+        System.out.println("Login success");
+    }
+
+    public String getName(){
+        return this.nickname;
+    }
+
+    public static void main(String[] args){
+        try{
+            ClientRMI clientRMI = new ClientRMI("marco");
+            clientRMI.login("127.0.0.1", 1099);
+        }catch (RemoteException re){
+            System.out.println(re.toString()+"no ok");
+            re.fillInStackTrace();
+        }
+    }
 }
