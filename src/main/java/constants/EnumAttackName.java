5a7b9cedc10f4655550a4b7873b7ea15cd8b2a8a
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-06-15 19:16:26 +0200

    finish label for MACHINE_GUN

--- src/main/java/constants/EnumAttackName.java
+++ src/main/java/constants/EnumAttackName.java
@@ -8,8 +8,9 @@ public enum EnumAttackName {
     TEST_ATTACK("Test attack",""),
 
     LOCK_RIFLE_OPT1("With second lock","Deal 1 mark to a different target you can see."),
-    MACHINE_GUN_OP1("With focus shot","Deal 1 additional damage to one of those targets."),
-    MACHINE_GUN_OP2("With turret tripod","Deal 1 additional damage to the other od those targets and/or deal 1 damage to a different target you can see."),
+    MACHINE_GUN_BASE("Base Attack","Deal 1 damage to two different target"),
+    MACHINE_GUN_OP1("With focus shot","Deal 1 additional damage to the first of those targets."),
+    MACHINE_GUN_OP2("With turret tripod","Deal 1 additional damage to the second of those targets and/or deal 1 damage to a third target you can see."),
     ELECTROSCYHE_OPT1("Reaper mode","Deal 2 damage to every other player on your square."),
     TRACTOR_BEAN_OPT1("Punisher mode","Chose a target 0,1 or 2 moves away from you. Move the target to your square and deal 3 damage to it."),
     VORTEX_CANNON_OPT1("With black hole","Chose up 2 other targets on the vortex or 1 move away from it. Move them onto the vortex and give them each 1 damage."),
