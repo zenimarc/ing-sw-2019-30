4dd58d7b3f4806f8be4f36ce02b5868ff4391c79
gioPez <giovanni.pezzoni@mail.polimi.it>
2019-06-16 23:10:30 +0200

    add some label

--- src/main/java/constants/EnumAttackName.java
+++ src/main/java/constants/EnumAttackName.java
@@ -7,6 +7,7 @@ public enum EnumAttackName {
     SUPPORT_ATTACK("LOREM IPSUM",""),
     TEST_ATTACK("Test attack",""),
 
+    LOCK_RIFLE_BASE("Base Attack","Deal 2 damage and 1 mark to a target you can see."),
     LOCK_RIFLE_OPT1("With second lock","Deal 1 mark to a different target you can see."),
     MACHINE_GUN_BASE("Base Attack","Deal 1 damage to two different target"),
     MACHINE_GUN_OP1("With focus shot","Deal 1 additional damage to the first of those targets."),
@@ -14,6 +15,7 @@ public enum EnumAttackName {
     ELECTROSCYHE_OPT1("Reaper mode","Deal 2 damage to every other player on your square."),
     TRACTOR_BEAN_OPT1("Punisher mode","Chose a target 0,1 or 2 moves away from you. Move the target to your square and deal 3 damage to it."),
     VORTEX_CANNON_OPT1("With black hole","Chose up 2 other targets on the vortex or 1 move away from it. Move them onto the vortex and give them each 1 damage."),
+    ZX_2_BASE("Base Attack","Deal 1 damage and 2 marks to 1 target you can see."),
     ZX_2_OP1("Scanner mode","Choose up 3 targets you can see and deal 1 mark to each."),
     HELLION_OPT1("Nano-tracer mode","Deal 1 damage to 1 target you can see at least 1 move away. Them give 2 marks to that target and everyone else on that square."),
     SHOCKWAVE_OPT1("Tsunami mode","lorem ipsum"),
